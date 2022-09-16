23dbb5f123740d5e4780ab6fe710efc028deb3b4
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-09-16 19:46:04 +0530

    feat: Added require colors and typography
    Signed-off-by: pranaypatel512 <www.pranay1337.com@gmail.com>

--- app/src/main/java/com/example/uberbookingexperience/ui/theme/Color.kt
+++ app/src/main/java/com/example/uberbookingexperience/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package com.example.uberbookingexperience.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+val colorBlack = Color(0XFF000000)
+val colorUberGrayBg= Color(0XFFF6F6F6)
+val colorWhite= Color(0XFFFFFFFF)
+val colorGrayLight= Color(0XFFBDBDBD)
+val colorGrayExtraLight= Color(0XFFE0E0E0)
+
+
 val TextGrey = Color(0x8A000000)
 val TextFieldUnFocusedGrey = Color(0xFFF5F5F5)
 val TextFieldFocusedGrey = Color(0xFFEEEEEE)
