fcb1ea330f56e4cf9f0c0c3d0624846bb3f04f93
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-09-19 17:31:11 +0530

    feat: Added schedule app icon and required todo
    Signed-off-by: pranaypatel512 <www.pranay1337.com@gmail.com>

--- app/src/main/java/com/example/uberbookingexperience/components/DefaultAppButton.kt
+++ app/src/main/java/com/example/uberbookingexperience/components/DefaultAppButton.kt
@@ -4,18 +4,21 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.uberbookingexperience.R
 import com.example.uberbookingexperience.ui.theme.colorBlack
-import com.example.uberbookingexperience.ui.theme.colorGrayExtraLight
 import com.example.uberbookingexperience.ui.theme.colorUberGrayBg
 import com.example.uberbookingexperience.ui.theme.colorWhite
 import com.example.uberbookingexperience.ui.util.UberIconSize.SizeButton
@@ -64,5 +67,5 @@ fun DefaultAppButtonPreview(){
 @Composable
 @Preview
 fun DefaultAppIconButtonPreview(){
-    DefaultAppIconButton(R.drawable.baseline_schedule_24){}
+    DefaultAppIconButton(R.drawable.schedule_button_icon) {}
 }
