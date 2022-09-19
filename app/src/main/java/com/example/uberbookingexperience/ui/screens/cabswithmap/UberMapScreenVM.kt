6c5e90b999e29b51f66ccb1ca1566f7b3f191a93
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-09-19 17:19:55 +0530

    refactor: Update ubermap screen viewmodel
    Signed-off-by: pranaypatel512 <www.pranay1337.com@gmail.com>

--- app/src/main/java/com/example/uberbookingexperience/ui/screens/cabswithmap/UberMapScreenVM.kt
+++ app/src/main/java/com/example/uberbookingexperience/ui/screens/cabswithmap/UberMapScreenVM.kt
@@ -1,10 +1,15 @@
 package com.example.uberbookingexperience.ui.screens.cabswithmap
 
 import android.location.Location
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.ViewModel
+import com.example.uberbookingexperience.R
+import com.example.uberbookingexperience.model.UberCabInfo
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import kotlin.random.Random
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.flow
 
 class UberMapScreenVM : ViewModel() {
@@ -32,4 +37,39 @@ class UberMapScreenVM : ViewModel() {
         }
         return location
     }
+
+    var selectedUberCab:UberCabInfo?=null
+    var isItemSelected= MutableStateFlow(false)
+
+    fun selectItem(selectedUberCabIndex: Int) {
+        cabListing.forEach {
+            it.isChecked = false
+        }
+        cabListing[selectedUberCabIndex] = cabListing[selectedUberCabIndex].copy(isChecked = true)
+        selectedUberCab=cabListing[selectedUberCabIndex]
+    }
+
+    var cabListing = mutableStateListOf(
+        UberCabInfo(
+            cabInfo = "Uber Pool", cabIcon =
+            R.drawable.ub__mode_nav_carpool,
+            cabPrice = 80.80f,
+            cabPriceAlter = 100.20f,
+            carTime = "3:09 PM"
+        ),
+        UberCabInfo(
+            cabInfo = "Uber Car",
+            cabIcon =
+             R.drawable.ub__mode_nav_ride,
+            cabPrice = 120.80f,
+            cabPriceAlter = 200.20f,
+            carTime = "1:19 PM",
+        ),
+        UberCabInfo(
+            cabInfo = "Uber Bike", cabIcon =
+             R.drawable.ub__mode_nav_bike_scooter,
+            cabPrice = 120.80f,
+            carTime = "11:19 AM"
+        )
+    )
 }
