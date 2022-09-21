f60efb50d233c85a197c1324f5e4e155bcaddd07
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-09-21 19:42:48 +0530

    refactor: Update UberMap viewmodel class
    Signed-off-by: pranaypatel512 <www.pranay1337.com@gmail.com>

--- app/src/main/java/com/example/uberbookingexperience/ui/screens/cabswithmap/UberMapScreenVM.kt
+++ app/src/main/java/com/example/uberbookingexperience/ui/screens/cabswithmap/UberMapScreenVM.kt
@@ -1,42 +1,12 @@
 package com.example.uberbookingexperience.ui.screens.cabswithmap
 
-import android.location.Location
-import androidx.compose.runtime.*
-import androidx.compose.ui.res.painterResource
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import com.example.uberbookingexperience.R
 import com.example.uberbookingexperience.model.UberCabInfo
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import kotlin.random.Random
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flow
 
 class UberMapScreenVM : ViewModel() {
-    val zoom = 8f
-    val mmLocation = LatLng(-31.673, 128.892)
-    val defaultCameraPosition = CameraPosition.fromLatLngZoom(mmLocation, zoom)
-    var startLocation: LatLng = LatLng(-31.673, 128.892)
-    var endLocation: LatLng =  LatLng(-12.4258, 130.7932)
-
-
-    val locationFlow = flow {
-        emit(newLocation())
-    }
-
-    fun setLocations(start: LatLng, end: LatLng) {
-        startLocation = start
-        endLocation = end
-    }
-
-    fun newLocation(): Location {
-        val location = Location("MyLocationProvider")
-        location.apply {
-            latitude = mmLocation.latitude + Random.nextFloat()
-            longitude = mmLocation.longitude + Random.nextFloat()
-        }
-        return location
-    }
 
     var selectedUberCab:UberCabInfo?=null
     var isItemSelected= MutableStateFlow(false)
