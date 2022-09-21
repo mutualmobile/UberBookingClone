2e9bc71bf25edcfd5e1e035f6cb9f7bd23383ef9
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-09-21 19:43:22 +0530

    feat: created reusable googlemap composable to reuse in uber app
    Signed-off-by: pranaypatel512 <www.pranay1337.com@gmail.com>

--- app/src/main/java/com/example/uberbookingexperience/components/UberGoogleMap.kt
+++ app/src/main/java/com/example/uberbookingexperience/components/UberGoogleMap.kt
@@ -1,10 +1,7 @@
 package com.example.uberbookingexperience.components
 
-import android.location.Location
 import android.util.Log
 import androidx.annotation.DrawableRes
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -12,40 +9,63 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.uberbookingexperience.R
-import com.example.uberbookingexperience.ui.screens.cabswithmap.UberMapScreenVM
+import com.example.uberbookingexperience.ui.util.rememberDeviceWidth
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.LocationSource
 import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.JointType
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.RoundCap
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.maps.android.compose.*
+import kotlinx.coroutines.CoroutineScope
 
 private const val TAG = "UberGoogleMap"
 
 @Composable
-fun UberGoogleMap(viewModel: UberMapScreenVM) {
+fun UberGoogleMap(
+    modifier: Modifier = Modifier,
+    scope: CoroutineScope = rememberCoroutineScope(),
+    cameraPositionState: CameraPositionState = rememberCameraPositionState(),
+    mapProperties: MapProperties = MapProperties(isMyLocationEnabled = true),
+    cameraPositionDefault: CameraPosition? = null,
+    latLngBounds: LatLngBounds? = null,
+    locationSource: LocationSource? = null,
+    defaultZoomAnimation: Int = 1_000,
+    defaultZoom: Float = 8f,
+    zoomPadding: Int = ((rememberDeviceWidth() * 5) / 50),
+    content: (@Composable @GoogleMapComposable () -> Unit)? = null
+) {
 
-    val locationSource = UberMapLocationSource()
-    var isMapLoaded by remember { mutableStateOf(false) }
-    val cameraPositionState = rememberCameraPositionState {
-        position = viewModel.defaultCameraPosition
+
+    val isMapReady = remember {
+        mutableStateOf(false)
     }
-    val mapProperties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true)) }
 
-    val locationState = viewModel.locationFlow.collectAsState(initial = viewModel.newLocation())
-
-    LaunchedEffect(key1 = locationState.value) {
-        locationSource.onLocationChanged(locationState.value)
-
+    LaunchedEffect(isMapReady) {
         Log.d(TAG, "Updating camera position...")
-        val cameraPosition = CameraPosition.fromLatLngZoom(LatLng(locationState.value.latitude, locationState.value.longitude), viewModel.zoom)
-        cameraPositionState.animate(CameraUpdateFactory.newCameraPosition(cameraPosition), 1_000)
+        if (isMapReady.value) {
+            //val cameraPosition = CameraPosition.fromLatLngZoom(LatLng(locationState.value.latitude, locationState.value.longitude), viewModel.zoom)
+            if (cameraPositionDefault != null) {
+                cameraPositionState.animate(
+                    CameraUpdateFactory.newCameraPosition(
+                        cameraPositionDefault
+                    )
+                )
+            }
+            if (cameraPositionDefault == null && latLngBounds != null) {
+                cameraPositionState.animate(
+                    CameraUpdateFactory.newLatLngBounds(
+                        latLngBounds,
+                        zoomPadding
+                    ), defaultZoomAnimation
+                )
+
+            }
+        }
+
+
     }
     // Detect when the map starts moving and print the reason
     LaunchedEffect(cameraPositionState.isMoving) {
@@ -57,48 +77,22 @@ fun UberGoogleMap(viewModel: UberMapScreenVM) {
         }
     }
 
-    val infiniteTransition = rememberInfiniteTransition()
-    val animateColor by infiniteTransition.animateColor(
-        initialValue = Color(0xFFF4F4F4),
-        targetValue = Color(0xFF000000),
-        animationSpec = infiniteRepeatable(
-            animation = tween(
-                durationMillis = 2000,
-                easing = LinearEasing
-            ),
-            repeatMode = RepeatMode.Reverse
-        )
-    )
-    Box(Modifier.fillMaxSize()) {
+
+    Box(modifier.fillMaxSize()) {
         GoogleMap(
             modifier = Modifier.matchParentSize(),
             cameraPositionState = cameraPositionState,
             onMapLoaded = {
-                isMapLoaded = true
+                //Map loaded callback
+                isMapReady.value = true
             },
             locationSource = locationSource,
             properties = mapProperties
         ) {
+            if (content != null) {
+                content()
+            }
 
-            MarkerInfoWindowContent(MarkerState(viewModel.startLocation)) {
-                TextViewWithEndIcon("My start Location", R.drawable.baseline_navigate_next_24)
-            }
-            MarkerInfoWindowContent(MarkerState(viewModel.endLocation)) {
-                TextViewWithEndIcon("My end Location", R.drawable.baseline_navigate_next_24)
-            }
-            Polyline(
-                points = listOf(
-                    LatLng(-31.673, 128.892),
-                    LatLng(-31.952, 115.857),
-                    LatLng(-17.785, 122.258),
-                    LatLng(-12.4258, 130.7932)
-                ),
-                color = animateColor,
-                clickable = true,
-                startCap = RoundCap(),
-                endCap = RoundCap(),
-                jointType = JointType.ROUND
-            )
         }
         //Open this if you want show progress indicator till map loading
         /*if (!isMapLoaded) {
@@ -116,23 +110,6 @@ fun UberGoogleMap(viewModel: UberMapScreenVM) {
                 )
             }
         }*/
-    }
-}
-
-private class UberMapLocationSource : LocationSource {
-
-    private var listener: LocationSource.OnLocationChangedListener? = null
-
-    override fun activate(listener: LocationSource.OnLocationChangedListener) {
-        this.listener = listener
-    }
-
-    override fun deactivate() {
-        listener = null
-    }
-
-    fun onLocationChanged(location: Location) {
-        listener?.onLocationChanged(location)
     }
 }
 
