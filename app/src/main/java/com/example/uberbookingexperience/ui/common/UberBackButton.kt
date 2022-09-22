3c0da979b669ab8f8d9b64a00f18285e692d4340
pranaypatel512 <www.pranay1337.com@gmail.com>
2022-09-22 10:43:12 +0530

    feat: Added Uber back icon
    Signed-off-by: pranaypatel512 <www.pranay1337.com@gmail.com>

---
+++ app/src/main/java/com/example/uberbookingexperience/ui/common/UberBackButton.kt
@@ -0,0 +1,57 @@
+package com.example.uberbookingexperience.ui.common
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.uberbookingexperience.R
+import com.example.uberbookingexperience.ui.theme.colorWhite
+import com.example.uberbookingexperience.ui.util.UberIconSize.LargeButton
+
+/**
+ * An Uber-styled back button that helps its users for back navigation from screen.
+ * @param modifier the [Modifier] to be applied to this button
+ * @param iconId paint resource id of icon
+ * @param backgroundColor icon button background color
+ * @param contentDescription icon content Description
+ * @param onClick called when this button is clicked
+ * */
+@Composable
+fun UberBackButton(
+    modifier: Modifier = Modifier,
+    @DrawableRes iconId: Int,
+    backgroundColor: Color = colorWhite,
+    contentDescription: String = "",
+    onClick: () -> Unit
+) {
+    IconButton(
+        modifier = modifier
+            .size(52.dp)
+            .background(backgroundColor, CircleShape)
+            .padding(2.dp),
+        onClick = onClick
+    ) {
+        Icon(
+            painterResource(id = iconId),
+            contentDescription = contentDescription,
+            modifier = Modifier.size(LargeButton)
+        )
+    }
+}
+
+@Composable
+@Preview
+fun UberBackButtonPreview() {
+    UberBackButton(iconId = R.drawable.baseline_arrow_back_24) {
+
+    }
+}
