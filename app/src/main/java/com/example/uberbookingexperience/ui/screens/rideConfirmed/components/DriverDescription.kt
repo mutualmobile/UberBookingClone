6fd65dc3ea5e54ab9fe4669a937211d8d9884281
Shubham Singh <shubham.singh@mutualmobile.com>
2022-09-23 08:21:13 +0530

    Fix carName not aligning to end on screen size change

--- app/src/main/java/com/example/uberbookingexperience/ui/screens/rideConfirmed/components/DriverDescription.kt
+++ app/src/main/java/com/example/uberbookingexperience/ui/screens/rideConfirmed/components/DriverDescription.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.uberbookingexperience.ui.theme.spacing
@@ -109,7 +110,11 @@ fun DriverDescription(
                     style = MaterialTheme.typography.headlineMedium,
                     fontWeight = FontWeight.Medium
                 )
-                Text(text = carName, style = MaterialTheme.typography.bodyMedium)
+                Text(
+                    text = carName,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.End
+                )
             }
         }
         CompositionLocalProvider(
