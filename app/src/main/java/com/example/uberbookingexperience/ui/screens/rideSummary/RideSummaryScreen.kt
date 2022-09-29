77f5cabf38080a464f99dc31e2c3de7ad58bfb0f
subirMM <subir.chakraborty@mutualmobile.com>
2022-09-29 10:53:34 +0530

    created RideSummaryScreen

---
+++ app/src/main/java/com/example/uberbookingexperience/ui/screens/rideSummary/RideSummaryScreen.kt
@@ -0,0 +1,77 @@
+package com.example.uberbookingexperience.ui.screens.rideSummary
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.uberbookingexperience.ui.common.UberButton
+import com.example.uberbookingexperience.ui.common.UberDivider
+import com.example.uberbookingexperience.ui.screens.rideSummary.components.LocationDescription
+import com.example.uberbookingexperience.ui.screens.rideSummary.components.PaymentDescription
+import com.example.uberbookingexperience.ui.theme.UberBookingExperienceTheme
+import com.example.uberbookingexperience.ui.theme.spacing
+import com.example.uberbookingexperience.ui.util.DevicePreviews
+import com.example.uberbookingexperience.ui.util.limitWidth
+import com.example.uberbookingexperience.ui.util.rememberIsMobileDevice
+
+@Composable
+fun RideSummaryScreen() {
+    Column(
+        modifier = Modifier
+            .then(
+                if (rememberIsMobileDevice()) {
+                    Modifier.fillMaxWidth()
+                } else {
+                    Modifier.width(300.dp)
+                }
+            )
+            .wrapContentSize()
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Surface {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(MaterialTheme.spacing.medium)
+            ) {
+                LocationDescription("Upcoming Ride","Dev Arc Commercial Complex", "Fri, 30 Sep 14:16 IST")
+                UberDivider()
+                PaymentDescription(tentativeEstimate = "₹404-486")
+                UberButton(
+                    modifier = Modifier
+                        .padding(vertical = MaterialTheme.spacing.large)
+                        .limitWidth(),
+                    text = "Done",
+                    onClick = { }
+                )
+            }
+        }
+    }
+}
+
+@DevicePreviews
+@Composable
+private fun RideSummaryScreenPreview() {
+    UberBookingExperienceTheme {
+        Column(
+            modifier = Modifier.then(
+                if (rememberIsMobileDevice()) {
+                    Modifier.fillMaxWidth()
+                } else {
+                    Modifier.width(300.dp)
+                }
+            )
+        ) {
+            RideSummaryScreen()
+        }
+    }
+}
